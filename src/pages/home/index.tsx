f4e29dab1987a2c5ea0851c22e666ec8e1bb0777
santa-developer <finalatro37@naver.com>
2025-02-04 22:47:17 +0900

    feat: home화면 마크업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      {/* header */}
+      <header>
+        <div>
+          <Link to='/posts/new'>글쓰기</Link>
+          <Link to='/posts'>게시글</Link>
+          <Link to='/profile'>프로필</Link>
+        </div>
+      </header>
+      {/* nav */}
+      <div className='post__navigation'>
+        <div className='post__navigation--active'>전체</div>
+        <div>내 글</div>
+      </div>
+      {/* post list */}
+      <div className='post__list'>
+        {[...Array(10)].map((e, i) => (
+          <div key={i} className='post__box'>
+            <Link to={`/posts/${i}`}>
+              <div className='post__profile-box'>
+                <div className='post__profile'></div>
+                <div className='post__author-name'>fast campus</div>
+                <div className='post__date'>2025.02.04</div>
+              </div>
+              <div className='post__title'>게시글 {i + 1}</div>
+              <div className='post__text'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                consectetur ipsum non justo molestie, at placerat tellus
+                faucibus. Donec non condimentum odio, vel bibendum neque. Sed
+                vulputate, quam id tincidunt malesuada, mauris velit consectetur
+                lectus, in iaculis nisi nisi ac arcu.
+              </div>
+              <div className='post__utils-box'>
+                <div className='post__delete'>삭제</div>
+                <div className='post__edit'>수정</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+      {/* footer */}
+      <footer>
+        <Link to='/posts/new'>글쓰기</Link>
+        <Link to='/posts'>게시글</Link>
+        <Link to='/profile'>프로필</Link>
+      </footer>
+    </div>
+  );
 };
 
 export default Home;
