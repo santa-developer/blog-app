ac5c2ff84041f90860e74333f0e659893e732f0c
santa-developer <finalatro37@naver.com>
2025-02-07 16:20:08 +0900

    fix: 상대경로 -> 절대경로 변경

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import Home from "../pages/home";
-import PostList from "../pages/posts";
-import Postdetail from "../pages/posts/detail";
-import PostNew from "../pages/posts/new";
-import PostEdit from "../pages/posts/edit";
-import ProfilePage from "../pages/profile";
-import LoginPage from "../pages/login";
-import SignUpPage from "../pages/signup";
+import Home from "pages/home";
+import PostList from "pages/posts";
+import Postdetail from "pages/posts/detail";
+import PostNew from "pages/posts/new";
+import PostEdit from "pages/posts/edit";
+import ProfilePage from "pages/profile";
+import LoginPage from "pages/login";
+import SignUpPage from "pages/signup";
 
 export default function Router() {
   return (
