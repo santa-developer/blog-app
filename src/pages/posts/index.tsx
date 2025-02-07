2f3efd4e26c7a3f87716c9f0445943142facf531
santa-developer <finalatro37@naver.com>
2025-02-07 16:14:32 +0900

    feat: 게시글 페이지, 글쓰기 페이지 추가 마크업

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "components/Header";
+import PostList from "components/PostList";
+import Footer from "components/Footer";
 
 const PostPage = () => {
-  return <div>PostList</div>;
+  return (
+    <>
+      <Header />
+      <PostList hasNavigaion={false} />
+      <Footer />
+    </>
+  );
 };
 
 export default PostPage;
