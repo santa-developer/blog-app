3d6f45bac62b8b52d6fc82bc5005216f07e7ebb6
santa-developer <finalatro37@naver.com>
2025-02-07 22:46:47 +0900

    fix: 상세 페이지 "수정"버튼 활성화

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const PostDetail = () => {
   return (
@@ -15,7 +15,9 @@ const PostDetail = () => {
         </div>
         <div className='post__utils-box'>
           <div className='post__delete'>삭제</div>
-          <div className='post__edit'>수정</div>
+          <div className='post__edit'>
+            <Link to={`/posts/edit/1`}>수정</Link>
+          </div>
         </div>
         <div className='post__text'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
