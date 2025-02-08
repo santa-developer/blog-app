01194bc3938dba4bea321848ad9b9ed22e9c8948
santa-developer <finalatro37@naver.com>
2025-02-08 23:37:13 +0900

    feat: 토스트 설정 및 인증여부 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import Router from "./components/Router";
+import { app } from "firebaseApp";
+import { getAuth } from "firebase/auth";
+import { ToastContainer } from "react-toastify";
 
 function App() {
+  const auth = getAuth(app);
+  console.log(auth);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
   return (
     <>
-      <Router />
+      <ToastContainer />
+      <Router isAuthenticated={isAuthenticated} />
     </>
   );
 }
