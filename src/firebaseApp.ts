5a30fd58fcb7d390bea9e0bb97a051c42d2fa03a
santa-developer <finalatro37@naver.com>
2025-02-09 16:29:56 +0900

    conf: firestore 초기화

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, FirebaseApp, getApp } from "firebase/app";
 import "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,3 +25,6 @@ try {
 // const firebase = initializeApp(firebaseConfig);
 
 // export default firebase;
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
