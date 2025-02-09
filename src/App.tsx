9296b77d481c6f96bbcb30dd5f9098bd52ac3009
santa-developer <finalatro37@naver.com>
2025-02-09 14:07:09 +0900

    feat: 로그인 상태 변화 감지 추가 및 Loader 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Router from "./components/Router";
 import { app } from "firebaseApp";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
+import Loader from "components/Loader";
 
 function App() {
   const auth = getAuth(app);
-  console.log(auth);
+
+  // auth를 체크하기 전에(= initialized 전)는 loader를 띄어주는 용도
+  const [init, setInit] = useState<boolean>(false);
+  // auth의 currentUser가 있으면 uathenticated로 변경
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
+
+  // currentUser 값이 실시간으로 업데이트
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        setIsAuthenticated(true);
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
   return (
     <>
       <ToastContainer />
-      <Router isAuthenticated={isAuthenticated} />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </>
   );
 }
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -8,19 +8,36 @@ import ProfilePage from "pages/profile";
 import LoginPage from "pages/login";
 import SignUpPage from "pages/signup";
 
-export default function Router() {
+interface RouterProps {
+  isAuthenticated: boolean;
+}
+
+export default function Router({ isAuthenticated }: RouterProps) {
+  // firebase Auth 인증 상태 여부(boolean)에 따른 상태관리
+
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/posts' element={<PostList />} />
-        <Route path='/posts/:id' element={<Postdetail />} />
-        <Route path='/posts/new' element={<PostNew />} />
-        <Route path='/posts/edit/:id' element={<PostEdit />} />
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='*' element={<Navigate replace to='/' />} />
+        {isAuthenticated ? (
+          // 로그인 사용자 전용 라우트
+          <>
+            <Route path='/' element={<Home />} />
+            <Route path='/posts' element={<PostList />} />
+            <Route path='/posts/:id' element={<Postdetail />} />
+            <Route path='/posts/new' element={<PostNew />} />
+            <Route path='/posts/edit/:id' element={<PostEdit />} />
+            <Route path='/profile' element={<ProfilePage />} />
+            <Route path='*' element={<Navigate replace to='/' />} />
+          </>
+        ) : (
+          // 비로그인 사용자 전용 라우트
+          <>
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/signup' element={<SignUpPage />} />
+            {/* path='*': 존재하지 않는 경로로 이동하면 로그인 페이지로 리다이렉트. */}
+            <Route path='*' element={<LoginPage />} />
+          </>
+        )}
       </Routes>
     </>
   );
