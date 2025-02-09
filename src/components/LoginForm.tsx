da4fd230164ffe07cfdf09004e915896de8a3021
santa-developer <finalatro37@naver.com>
2025-02-09 14:05:46 +0900

    feat: 로그인 기능 구현

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,17 +1,92 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { app } from "firebaseApp";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const navigate = useNavigate();
+
+  // 입력값 변경 핸들러
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") {
+      setEmail(value);
+      const validRegex =
+        /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
+
+      if (!value?.match(validRegex)) {
+        setError("이메일 형식이 올바르지 않습니다.");
+      } else {
+        setError("");
+      }
+    }
+    if (name === "password") {
+      setPassword(value);
+
+      if (value.length < 8) {
+        setError("비밀번호는 8자리 이상 입력해주세요.");
+      } else {
+        setError("");
+      }
+    }
+  };
+  console.log(app);
+
+  // 폼 제출 핸들러
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // 기본 동작을 하지 않도록 작업
+
+    try {
+      const auth = getAuth(app);
+      await signInWithEmailAndPassword(auth, email, password);
+      setError("");
+
+      toast.success("로그인 하였습니다.");
+      navigate("/");
+    } catch (error: any) {
+      toast.error(error.code);
+      console.log(error);
+    }
+  };
   return (
-    <form action='/post' method='POST' className='form form--lg'>
+    // <form action='/post' method='POST' className='form form--lg'>
+    <form onSubmit={onSubmit} className='form form--lg'>
       <h2 className='form__title'>로그인</h2>
       <div className='form__block'>
         <label htmlFor='email'>이메일</label>
-        <input type='email' name='email' id='email' required />
+        <input
+          type='email'
+          name='email'
+          value={email}
+          id='email'
+          required
+          onChange={onChange}
+        />
       </div>
       <div className='form__block'>
         <label htmlFor='password'>비밀번호</label>
-        <input type='password' name='password' id='password' required />
+        <input
+          type='password'
+          name='password'
+          value={password}
+          id='password'
+          required
+          onChange={onChange}
+        />
       </div>
+      {error && error?.length > 0 && (
+        <div className='form__block'>
+          <div className='form__error'>{error}</div>
+        </div>
+      )}
       <div className='form__block'>
         계정이 없으신가요?
         <Link to='/signup' className='form__link'>
@@ -19,7 +94,12 @@ const LoginForm = () => {
         </Link>
       </div>
       <div className='form__block'>
-        <input type='submit' value='로그인' className='form__btn--submit' />
+        <input
+          type='submit'
+          value='로그인'
+          className='form__btn--submit'
+          disabled={error?.length > 0}
+        />
       </div>
     </form>
   );
