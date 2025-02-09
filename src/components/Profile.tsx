fec6f2c81105af5ae053cd0b58abf69beceee97b
santa-developer <finalatro37@naver.com>
2025-02-09 14:07:56 +0900

    feat: 로그아웃 기능 추가

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,20 +1,40 @@
-import React from "react";
-import Header from "./Header";
-import { Link } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Profile = () => {
+  const auth = getAuth(app);
+  const navigate = useNavigate();
+
+  const logOutHandler = async () => {
+    try {
+      await signOut(auth);
+      toast.success("로그아웃 되었습니다.");
+      navigate("/login");
+    } catch (error: any) {
+      toast.error(error.code);
+      console.log(error);
+    }
+  };
   return (
     <div className='profile__box'>
       <div className='flex__box-lg'>
         <div className='profile__image'></div>
         <div>
-          <div className='profile__email'>test@gmail.com</div>
-          <div className='profile__name'>santa</div>
+          <div className='profile__email'>{auth?.currentUser?.email}</div>
+          <div className='profile__name'>
+            {auth?.currentUser?.displayName || "사용자"}
+          </div>
         </div>
       </div>
-      <Link to='/' className='profile__logout'>
+      <div
+        role='presentation'
+        className='profile__logout'
+        onClick={logOutHandler}
+      >
         로그아웃
-      </Link>
+      </div>
     </div>
   );
 };
