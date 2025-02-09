f047bb42a27b213d57684b95c955710ec6e4386b
santa-developer <finalatro37@naver.com>
2025-02-09 22:14:39 +0900

    feat: 게시글 상세 가져오기 Firestore의 getDoc활용하여 기능 구현

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,32 +1,73 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { PostProps } from "./PostList";
+import { db } from "firebaseApp";
+import Loader from "./Loader";
+import AuthContext from "context/AuthContext";
 
 const PostDetail = () => {
+  const { user } = useContext(AuthContext);
+  const params = useParams();
+  // console.log(params?.id); // id 잘 넘어오는지 확인
+
+  const [post, setPost] = useState<PostProps | null>(null);
+
+  // 문서 가져오기(id를 받아서 해당 ID의 문서를 Firebase Firestore에서 가져온다.)
+  const getPosts = async (id: string) => {
+    try {
+      if (id) {
+        const docRef = doc(db, "posts", id);
+        const docSnap = await getDoc(docRef);
+
+        setPost({ ...docSnap.data(), id: docSnap.id } as PostProps);
+
+        console.log(docSnap.data());
+      }
+    } catch (e) {
+      console.error("게시글을 불러오는 중 오류가 발생했습니다.:", e);
+    }
+  };
+
+  // 삭제 이벤트
+  const handleDelete = () => {
+    console.log("delete!");
+  };
+
+  // 페이지가 마운트 될 때마다 document를 가져오는 함수를 실행
+  useEffect(() => {
+    // params의 ID 값이 있을 때만 getPosts 호출을 해야 한다.
+    if (params?.id) getPosts(params?.id);
+  }, [params?.id]);
   return (
     <div className='post__detail'>
-      <div className='post__box'>
-        <div className='post__title'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
-        </div>
-        <div className='post__profile-box'>
-          <div className='post__profile'></div>
-          <div className='post__author-name'>fast campus</div>
-          <div className='post__date'>2025.02.04</div>
-        </div>
-        <div className='post__utils-box'>
-          <div className='post__delete'>삭제</div>
-          <div className='post__edit'>
-            <Link to={`/posts/edit/1`}>수정</Link>
+      {post ? (
+        <div className='post__box'>
+          <div className='post__title'>{post?.title}</div>
+          <div className='post__profile-box'>
+            <div className='post__profile'></div>
+            <div className='post__author-name'>{post?.email}</div>
+            <div className='post__date'>{post?.createAt}</div>
           </div>
+          {user?.email === post?.email && (
+            <div className='post__utils-box'>
+              <div className='post__edit'>
+                <Link to={`/posts/edit/1`}>수정</Link>
+              </div>
+              <div
+                className='post__delete'
+                role='presentation'
+                onClick={handleDelete}
+              >
+                삭제
+              </div>
+            </div>
+          )}
+          <div className='post__text post__text--pre-wrap'>{post?.content}</div>
         </div>
-        <div className='post__text'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          consectetur ipsum non justo molestie, at placerat tellus faucibus.
-          Donec non condimentum odio, vel bibendum neque. Sed vulputate, quam id
-          tincidunt malesuada, mauris velit consectetur lectus, in iaculis nisi
-          nisi ac arcu.
-        </div>
-      </div>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
