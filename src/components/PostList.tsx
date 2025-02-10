a2eb4c5e1f5c3f945914e2e53635a4e8f1d350ec
santa-developer <finalatro37@naver.com>
2025-02-11 00:59:40 +0900

    feat: 댓글 interface 추가

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -27,6 +27,14 @@ export interface PostProps {
   updatedAt: string;
   uid: string;
   category?: CategoryType;
+  comments?: CommentsInterface[];
+}
+
+export interface CommentsInterface {
+  content: string;
+  uid: string;
+  email: string;
+  createdAt: string;
 }
 
 export type CategoryType = "FE" | "BE" | "WEB" | "APP";
